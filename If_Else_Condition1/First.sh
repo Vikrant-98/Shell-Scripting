bddfce2369497e79b037213624daf0c0422672ba
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-17 12:24:57 +0530

    Single Digit number in words

---
+++ If_Else_Condition1/First.sh
@@ -0,0 +1,43 @@
+#!/bin/bash -x
+
+read -p "Enter Single digit Number :" input
+
+if [ $input == 0 ]
+then
+	echo "Zero"
+elif [ $input == 1 ]
+then
+	echo "One"
+
+elif [ $input == 2 ]
+then
+	echo "Two"
+
+elif [ $input == 3 ]
+then
+	echo "Three"
+
+elif [ $input == 4 ]
+then
+	echo "Foue"
+
+elif [ $input == 5 ]
+then
+	echo "Five"
+
+elif [ $input == 6 ]
+then
+	echo "Six"
+
+elif [ $input == 7 ]
+then
+	echo "Seven"
+
+elif [ $input == 8 ]
+then
+	echo "Eight"
+
+elif [ $input == 9 ]
+then
+	echo "Nine"
+fi
