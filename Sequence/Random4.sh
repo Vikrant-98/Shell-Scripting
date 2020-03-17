5351d1c1db8a902d2bbd6cca43f4e9f48cde1995
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-17 12:06:53 +0530

    % Random 2 Digit Value and find Sum and Average

---
+++ Sequence/Random4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+count=0
+val1=0
+while [ $count != 5 ]
+do
+	val=$((RANDOM%100))
+	if [ $((val/10)) -ge 1 ]
+	then
+		echo $val
+		val1=$((val1+val))
+		((count++))
+	fi
+done
+avg=$(echo "scale=2; $val1/$count" | bc )
+echo "Sum is $val1"
+echo "Average is $avg"
