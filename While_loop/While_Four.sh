2a5e402b2ed2c1823e82d6c3e3bb136b5af9f4ce
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-03-18 09:57:38 +0530

    Gamblors Game git add While_loop/While_Four.sh git add While_loop/While_Four.sh !

---
+++ While_loop/While_Four.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+Gamblor=100
+counter1=0
+counter2=0
+echo "Gamblors initially amount is : $Gamblor "
+echo ""
+while (( $Gamblor != 0 && $Gamblor != 200 ))
+do
+val=$((RANDOM%2))
+
+	if [ $val -eq 0 ]
+	then
+		((Gamblor++))
+		((counter1++))
+	else
+		((Gamblor--))
+		((counter2++))
+	fi
+done
+if (( $Gamblor == 0 ))
+then
+	echo "Gamblor Win 200 !!!!"
+else
+	echo "Gamblor loose 0!!!!"
+fi
+echo ""
+echo "Gamblor wins $counter1 times"
+echo ""
+echo "Gamblor looses $counter2 times"
